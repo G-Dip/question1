69c19f54ffc0893be1275cac6c80a7953295880e
Iamdip96 <iamdip96@gmail.com>
2020-07-07 11:31:56 +0800

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,24 @@ void main() => runApp(MaterialApp(
       home: Home(),
     ));
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  String _modeName = "Light Mode";
+  String _colorName = "white";
+  get modeName => _modeName;
+  get colorName => _colorName;
+
+  set modeName(value) => setState(() {
+        _modeName = value;
+      });
+
+  set colorName(value) => setState(() {
+        _colorName = value;
+      });
   @override
   Widget build(BuildContext context) {
     return Scaffold(
